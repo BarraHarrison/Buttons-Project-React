869e440adeeacddd98009d88d10e15250e0ca628
Barra Harrison <143306095+BarraHarrison@users.noreply.github.com>
2024-08-14 13:32:27 +0900

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+import Button from './components/ButtonComponent/Button';
+
+function App() {
+  return (
+    <>
+      <Button label="Button 1" />
+      <Button label="Button 2" />
+      <Button label="Button 3" />
+      <Button label="Button 4" />
+      <Button label="Button 5" />
+      <Button label="Button 6" />
+    </>
+  );
+}
+
+export default App;
